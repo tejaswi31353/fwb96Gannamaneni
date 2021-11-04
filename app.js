e5bcba8c39e76fe8ccfcd42045e23e3f63650753
tejaswi31353 <89408801+tejaswi31353@users.noreply.github.com>
2021-11-03 19:33:44 -0500

    updated files

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helicopterRouter = require('./routes/helicopter');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/helicopter', helicopterRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Helicopter.js
+++ routes/Helicopter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Helicopter', { title: 'Search Results Helicopter' });
+  res.render('helicopter', { title: 'Search Results helicopter' });
 });
 
 module.exports = router;
